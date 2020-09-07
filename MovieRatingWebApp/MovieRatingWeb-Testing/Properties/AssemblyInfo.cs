7de5b8a8257176e7ce87a150a4abc581e41e0538
Aditya Garg <aditya.garg100220@gmail.com>
2020-09-07 22:48:02 +1200

    my first commit

---
+++ MovieRatingWebApp/MovieRatingWeb-Testing/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MovieRatingWeb-Testing")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MovieRatingWeb-Testing")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("49f3460f-658b-4292-8066-36992d3ec31d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MovieRatingWebApp/MovieRatingWebApp/wwwroot/js/movie-rating.js
@@ -0,0 +1,37 @@
+﻿$(function () {
+    $("img.rating").mouseover(function () {
+        //giveRating($(this), "star-filled.jpg");
+        //$(this).css("cursor", "pointer");
+    });
+
+    $("img.rating").mouseout(function () {
+        //giveRating($(this), "star-empty.png");
+        //refilRating($(this));
+    });
+
+    $("img.rating").click(function (e) {
+        $(this).css('color', 'red');
+        giveRating($(this), "star-filled.jpg");
+        $(this).css("cursor", "pointer");
+
+        // calls SaveRating method ( ajax ) to update the database
+        var url = "/Ratings/SaveRating?rate=" + parseInt($(this).attr("id")) + "&movieId=" + parseInt($(this).attr("movieId"));
+
+        $.post(url, null, function (data) {
+            console.log(data);
+
+        });
+
+    });
+
+});
+
+function giveRating(img, image) {
+    img.attr("src", "/Images/" + image).prevAll("img.rating").attr("src", "/Images/" + image);
+}
+
+function refilRating(img1) {
+    var rt = $(img1).closest('tr').find("span.avr").text();
+    var img = $(img1).closest('tr').find("img[id='" + parseInt(rt) + "']");
+    img.attr("src", "/images/star-filled.png").prevAll("img.rating").attr("src", "/images/star-filled.jpg");
+}
